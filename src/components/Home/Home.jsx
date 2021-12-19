1839540ddd18f5c12a5381332ac2917a55d5a11a
jjcasco99 <juanjocasco28@gmail.com>
2021-12-20 00:05:46 +0100

    cambio de footer y nada añadido al Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,30 +11,36 @@ const Home = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setPokeName(e.target.pokemon.value)
+    console.log(pokeName)
   }
-      
+
   useEffect(() => {
-    const getPokemons = async () =>{
+    const getPokemons = async () => {
+      try {
+
         const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
         setPokemons({
-          name:resp.data.name,
-          picture:resp.data.sprites.front_default,
+          name: resp.data.name,
+          picture: resp.data.sprites.front_default,
           id: resp.data.id
         });
+      } catch {
+        setPokemons([]);
+      }
     }
     getPokemons();
-  },[pokeName]);
+  }, [pokeName]);
 
-  
+
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="pokemon">Introduce un pokemon:</label><br />
-        <input type="text" id="pokemon" name="pokemon"/><br />
-        <input type="submit"/>
+        <input type="text" id="pokemon" name="pokemon" /><br />
+        <input type="submit" />
       </form>
-      <> {pokeName ? <Card pokemon={pokemons}/>: ""}</>
+      <> {pokeName ? <Card pokemon={pokemons} /> : ""}</>
     </div>
   );
 };
